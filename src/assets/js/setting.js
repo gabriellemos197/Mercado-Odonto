187323e4f1bad0e643a18a8b1f451ca9c484ca84
gabriellemos197 <contato@gabriellemos.com>
2018-11-23 08:57:09 -0200

    Initial commit

---
+++ src/assets/js/setting.js
@@ -0,0 +1,10 @@
+var wordpress_url = "http://mercadoodonto.com";
+var onesignal_app_id = "81fb2c48-9937-4d9a-8d8e-3d558f2faf0c";
+var google_web_api_key = "486944611348-ubf0mlrhtabosimclvohndc6r39qfo0g.apps.googleusercontent.com";
+var wordpress_per_page = 10;
+var post_per_page = 4;
+var request_timeout = 15000; // milliseconds 
+var notice_timeout = 2000; // milliseconds
+var slide_time = 3000; // milliseconds
+var hide_sale = true;
+var hide_available_stock = true;
